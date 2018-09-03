d17167ca99955188c35fcaadfcf0caeb0371d78e
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-09-03 20:00:11 +0200

    import jenkins.plugins.http_request.*

--- vars/Mainframe_CI_Pipeline_from_Shared_Lib.groovy
+++ vars/Mainframe_CI_Pipeline_from_Shared_Lib.groovy
@@ -4,6 +4,7 @@ import hudson.EnvVars
 import groovy.json.JsonSlurper
 import groovy.json.JsonBuilder
 import groovy.json.JsonOutput
+import jenkins.plugins.http_request.*
 import java.net.URL
 import com.compuware.devops.util.*
 
