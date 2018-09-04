d1d5548fcbaa5aa4d124c53b6559cef1aac398aa
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-09-04 08:22:53 +0200

    FTS Config

--- src/com/compuware/devops/util/PipelineConfig.groovy
+++ src/com/compuware/devops/util/PipelineConfig.groovy
@@ -8,11 +8,11 @@ Class to hold Jenkins server specific setup information
 */
 
 public String Git_Branch           = "master"
-public String SQ_Scanner_Name      = "Scanner" //"scanner" 
-public String SQ_Server_Name       = "CWCC" //"localhost"  
+public String SQ_Scanner_Name      = "scanner" //"Scanner" //"scanner" 
+public String SQ_Server_Name       = "localhost"  //"CWCC" //"localhost"  
 public String MF_Source            = "MF_Source"
-public String XLR_Template         = "A Release from Jenkins - RNU" //"A Release from Jenkins"
-public String XLR_User             = "xebialabs" //"admin"                           
+public String XLR_Template         = "A Release from Jenkins" //"A Release from Jenkins - RNU" //"A Release from Jenkins"
+public String XLR_User             = "admin"    //"xebialabs" //"admin"                           
 public String TTT_Folder           = "tests"	
 public String ISPW_URL             = "http://cwcc.compuware.com:2020"
 public String ISPW_Runtime         = "ispw"		 
