3d2d0b3d0d84f848bb2dc05cb191c8efa2ee4d1a
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-09-13 14:03:54 +0200

    Include push

--- vars/FTSDEMO_Checkout_Task.groovy
+++ vars/FTSDEMO_Checkout_Task.groovy
@@ -179,10 +179,8 @@ def call(Map pipelineParams)
                         [usernamePassword(credentialsId: "${Git_Credentials}", passwordVariable: 'gitPassword', usernameVariable: 'gitUsername')]
                     ) 
                     {              
-                        /*  
                         stdout = bat(returnStdout: true, script: "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${Git_Project}/${Git_TTT_Repo} HEAD:${gitNewBranch} -f --tags")
                         echo "pushed " + stdout
-                        */
                     }
 
                     emailBody = "Jenkins Job ${JOB_NAME} was executed because you checked out tasks to ISPW assignment ${gitNewBranch}." +
