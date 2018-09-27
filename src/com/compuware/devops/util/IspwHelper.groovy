010d027f78adf1586db8f7ba0b3339397bd9a477
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-09-27 23:40:54 +0200

    Parameters

--- src/com/compuware/devops/util/IspwHelper.groovy
+++ src/com/compuware/devops/util/IspwHelper.groovy
@@ -40,7 +40,7 @@ class IspwHelper implements Serializable
         def jsonSlurper = new JsonSlurper()
         def returnList  = []
 
-        def taskIds = getSetTaskIdList(cesToken, ispwUrl, ispwRuntime, ispwContainer)
+        def taskIds = getSetTaskIdList(cesToken)
 
         withCredentials(
             [string(credentialsId: "${cesToken}", variable: 'cesTokenVar')]
