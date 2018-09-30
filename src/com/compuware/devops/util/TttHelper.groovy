b750939e7adadb8693ea95f57e0e218d6068181b
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-09-30 09:44:57 +0200

    Complete commit

--- src/com/compuware/devops/util/TttHelper.groovy
+++ src/com/compuware/devops/util/TttHelper.groovy
@@ -1,56 +1,90 @@
 package com.compuware.devops.util
 
-/**
- Wrapper around the Git Plugin's Checkout Method
- @param URL - URL for the git server
- @param Branch - The branch that will be checked out of git
- @param Credentials - Jenkins credentials for logging into git
- @param Folder - Folder relative to the workspace that git will check out files into
-*/
-class GitHelper implements Serializable {
+class TttHelper implements Serializable {
 
     def steps
+    def pConfig
 
-    GitHelper(steps) 
+    def listOfScenarios
+    def listOfSources
+    def listOfPrograms 
+
+    TttHelper(steps, pConfig) 
     {
-        this.steps = steps
+        this.steps      = steps
+        this.pConfig    = pConfig
+
+        // findFiles method requires the "Pipeline Utilities Plugin"
+        // Get all testscenario files in the current workspace into an array
+        this.tttListOfScenarios  = findFiles(glob: '**/*.testscenario')
+
+        // Get all Cobol Sources in the MF_Source folder into an array 
+        this.listOfSources       = findFiles(glob: "**/${pConfig.ispwApplication}/${pConfig.mfSourceFolder}/*.cbl")
+
+        // Define a empty array for the list of programs
+        this.listOfPrograms      = []
+
+        // Determine program names for each source member
+        listOfSources.each
+        {
+            // The split method uses regex to search for patterns, therefore
+            // Backslashes, Dots and Underscores which mean certain patterns in regex need to be escaped 
+            // The backslash in Windows paths is duplicated in Java, therefore it need to be escaped twice
+            // Trim ./cbl from the Source members to populate the array of program names
+            listOfPrograms.add(it.name.trim().split("\\.")[0])
+        }
     }
 
-    def checkout(String gitUrl, String gitBranch, String gitCredentials, String tttFolder)
+    def loopThruScenarios()
     {
-        steps.checkout(
-            changelog:  false, 
-            poll:       false, 
-            scm:        [
-                        $class:                                 'GitSCM', 
-                            branches:                           [[name: "*/${gitBranch}"]], 
-                            doGenerateSubmoduleConfigurations:  false, 
-                            extensions:                         [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${tttFolder}"]], 
-                            submoduleCfg:                       [], 
-                            userRemoteConfigs:                  [[credentialsId: "${gitCredentials}", name: 'origin', url: "${gitUrl}"]]
-                        ]
-        )
+
+        // Loop through all downloaded Topaz for Total Test scenarios
+        tttListOfScenarios.each
+        {
+
+            // Get root node of the path, i.e. the name of the Total Test project
+            def tttScenarioPath        = it.path // Fully qualified name of the Total Test Scenario file
+            def tttProjectName         = it.path.trim().split("\\\\")[0] + "\\"+ it.path.trim().split("\\\\")[1]  // Total Test Project name is the root folder of the full path to the testscenario 
+            def tttScenarioFullName    = it.name  // Get the full name of the testscenario file i.e. "name.testscenario"
+            def tttScenarioName        = it.name.trim().split("\\.")[0]  // Get the name of the scenario file without ".testscenario"
+            def tttScenarioTarget      = tttScenarioName.split("\\_")[0]  // Target Program will be the first part of the scenario name (convention)
+    
+            // For each of the scenarios walk through the list of source files and determine if the target matches one of the programs
+            // In that case, execute the unit test.  Determine if the program name matches the target of the Total Test scenario
+            if(listOfPrograms.contains(tttScenarioTarget))
+            {
+                // Log which 
+                println "*************************\n" +
+                    "Scenario " + tttScenarioFullName + '\n' +
+                    "Path " + tttScenarioPath + '\n' +
+                    "Project " + tttProjectName + '\n' +
+                    "*************************"
+            
+                steps.step([
+                    $class:       'TotalTestBuilder', 
+                        ccClearStats:   false,                          // Clear out any existing Code Coverage stats for the given ccSystem and ccTestId
+                        ccRepo:         "${pConfig.ccRepository}",
+                        ccSystem:       "${pConfig.ispwApplication}", 
+                        ccTestId:       "${steps.BUILD_NUMBER}",              // Jenkins environment variable, resolves to build number, i.e. #177 
+                        credentialsId:  "${pConfig.hciTokenId}", 
+                        deleteTemp:     true,                           // (true|false) Automatically delete any temp files created during the execution
+                        hlq:            '',                             // Optional - high level qualifier used when allocation datasets
+                        connectionId:   "${pConfig.hciConnId}",    
+                        jcl:            "${pConfig.tttJcl}",            // Name of the JCL file in the Total Test Project to execute
+                        projectFolder:  "${tttProjectName}",            // Name of the Folder in the file system that contains the Total Test Project.  
+                        testSuite:      "${tttScenarioFullName}",       // Name of the Total Test Scenario to execute
+                        useStubs:       true                            // (true|false) - Execute with or without stubs
+                ])                   
+            }
+        }
     }
 
-    def checkoutPath(String gitUrl, String gitBranch, String path, String gitCredentials, String gitProject)
+    def passResultsToJunit()
     {
-        steps.checkout(
-        changelog: false, 
-        poll: false, 
-        scm: [
-                $class: 'GitSCM', 
-                branches: [[name: "*/${gitBranch}"]], 
-                doGenerateSubmoduleConfigurations: false, 
-                extensions: [[
-                    $class: 'SparseCheckoutPaths', 
-                    sparseCheckoutPaths: [[path: "${path}/*"]]
-                ]], 
-                submoduleCfg: [], 
-                userRemoteConfigs: [[
-                    credentialsId: "${gitCredentials}", 
-                    url: "${gitUrl}/${gitProject}.git"
-                ]]
-            ]
-        )
+        // Process the Total Test Junit result files into Jenkins
+        junit allowEmptyResults:    true, 
+            keepLongStdio:          true, 
+            testResults:            "TTTUnit/*.xml"
     }
+
 }
