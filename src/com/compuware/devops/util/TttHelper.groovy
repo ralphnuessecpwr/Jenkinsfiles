61018de1092da3bba807b3afa2ae3c9029e95d4f
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-10-01 17:21:14 +0200

    Escape \

--- src/com/compuware/devops/util/TttHelper.groovy
+++ src/com/compuware/devops/util/TttHelper.groovy
@@ -58,7 +58,7 @@ class TttHelper implements Serializable {
             // Get root node of the path, i.e. the name of the Total Test project
             def scenarioPath        = it.path // Fully qualified name of the Total Test Scenario file
             def projectName         = it.path.trim().split("\\\\")[0] + "\\"+ it.path.trim().split("\\\\")[1]  // Total Test Project name is the root folder of the full path to the testscenario 
-            def jclFolder           = projectName + '\\Unit Test\JCL'   // Path containing Runner.jcl
+            def jclFolder           = projectName + '\\Unit Test\\JCL'   // Path containing Runner.jcl
             def scenarioFullName    = it.name  // Get the full name of the testscenario file i.e. "name.testscenario"
             def scenarioName        = it.name.trim().split("\\.")[0]  // Get the name of the scenario file without ".testscenario"
             def scenarioTarget      = scenarioName.split("\\_")[0]  // Target Program will be the first part of the scenario name (convention)
