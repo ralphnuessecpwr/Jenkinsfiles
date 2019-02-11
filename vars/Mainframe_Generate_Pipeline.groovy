e5dc1ee82d4d2c23efa20c899a188815e4f1d55f
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2019-02-11 16:31:42 +0100

    Generate Pipeline

---
+++ vars/Mainframe_Generate_Pipeline.groovy
@@ -0,0 +1,167 @@
+#!/usr/bin/env groovy
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import jenkins.plugins.http_request.*
+import java.net.URL
+import com.compuware.devops.util.*
+
+/**
+ Helper Methods for the Pipeline Script
+*/
+PipelineConfig  pConfig
+GitHelper       gitHelper
+IspwHelper      ispwHelper
+TttHelper       tttHelper
+SonarHelper     sonarHelper 
+
+def ResponseContentSupplier response3
+//def assignmentList = []
+
+def initialize(pipelineParams)
+{
+    def mailListlines
+    /* Read list of mailaddresses from "private" Config File */
+    /* The configFileProvider creates a temporary file on disk and returns its path as variable */
+    configFileProvider(
+        [
+            configFile(
+                fileId: 'MailList', 
+                variable: 'mailListFilePath'
+            )
+        ]
+    ) 
+    {
+        File mailConfigFile = new File(mailListFilePath)
+
+        if(!mailConfigFile.exists())
+        {
+            steps.error "File - ${mailListFilePath} - not found! \n Aborting Pipeline"
+        }
+
+        mailListlines = mailConfigFile.readLines()
+    }
+
+    pConfig     = new   PipelineConfig(
+                            steps, 
+                            workspace,
+                            pipelineParams,
+                            mailListlines
+                        )
+
+    pConfig.initialize()                                            
+
+    gitHelper   = new   GitHelper(
+                            steps
+                        )
+
+    ispwHelper  = new   IspwHelper(
+                            steps, 
+                            pConfig
+                        )
+
+    tttHelper   = new   TttHelper(
+                            this,
+                            steps,
+                            pConfig
+                        )
+
+    sonarHelper = new SonarHelper(this, steps, pConfig)
+
+    sonarHelper.initialize()
+}
+
+/**
+Call method to execute the pipeline from a shared library
+@param pipelineParams - Map of paramter/value pairs
+*/
+def call(Map pipelineParams)
+{
+    node
+    {
+        stage("Initialization")
+        {
+            initialize(pipelineParams) 
+        }
+                
+        /* Download all sources that are part of the container  */
+        stage("Retrieve Mainframe Code")
+        {
+            ispwHelper.downloadSources()
+        //}
+        
+        /* Download all copybooks in case, not all copybook are part of the container  */
+        //stage("Retrieve Copy Books From ISPW")
+        //{
+            ispwHelper.downloadCopyBooks("${workspace}")
+        }
+        
+        /* Retrieve the Tests from Github that match that ISPWW Stream and Application */
+        stage("Execute Unit Tests")
+        {            
+            def gitUrlFullPath = "${pConfig.gitUrl}/${pConfig.gitTttRepo}"
+            
+            gitHelper.checkout(gitUrlFullPath, pConfig.gitBranch, pConfig.gitCredentials, pConfig.tttFolder)
+        //}
+
+        /* 
+        This stage executes any Total Test Projects related to the mainframe source that was downloaded
+        */ 
+        //stage("Execute related Unit Tests")
+        //{
+            tttHelper.initialize()                                            
+            tttHelper.loopThruScenarios()
+            //tttHelper.passResultsToJunit()
+        }
+
+        /* 
+        This stage retrieve Code Coverage metrics from Xpediter Code Coverage for the test executed in the Pipeline
+        */ 
+        stage("Collect Metrics")
+        {
+            tttHelper.collectCodeCoverageResults()
+        }
+
+        /* 
+        This stage pushes the Source Code, Test Metrics and Coverage metrics into SonarQube and then checks the status of the SonarQube Quality Gate.  
+        If the SonarQube quality date fails, the Pipeline fails and stops
+        */ 
+        stage("Check SonarQube Quality Gate") 
+        {
+            sonarHelper.scan()
+
+            // Wait for the results of the SonarQube Quality Gate
+            timeout(time: 2, unit: 'MINUTES') 
+            {                
+                // Wait for webhook call back from SonarQube.  SonarQube webhook for callback to Jenkins must be configured on the SonarQube server.
+                def sonarGate = waitForQualityGate()
+                
+                // Evaluate the status of the Quality Gate
+                if (sonarGate.status != 'OK')
+                {
+                    echo "Sonar quality gate failure: ${sonarGate.status}"
+                    echo "Pipeline will be aborted and ISPW Assignment will be regressed"
+
+                    currentBuild.result = "FAILURE"
+
+                    error "Exiting Pipeline" // Exit the pipeline with an error if the SonarQube Quality Gate is failing
+                }
+            }   
+        }
+
+        /* 
+        This stage triggers a XL Release Pipeline that will move code into the high levels in the ISPW Lifecycle  
+        */ 
+        stage("Send Mail")
+        {
+            // Send Standard Email
+            emailext subject:       '$DEFAULT_SUBJECT',
+                        body:       '$DEFAULT_CONTENT \n' + 'Promote passed the Quality gate and a new XL Release was started.',
+                        replyTo:    '$DEFAULT_REPLYTO',
+                        to:         "${pConfig.mailRecipient}"
+
+        }        
+    }
+}
