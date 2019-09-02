1fbc8d509fd3cecd13a2cb0c62244a5c88485a1b
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2019-09-02 19:22:39 +0200

    Corrected imports

--- vars/Test.groovy
+++ vars/Test.groovy
@@ -6,7 +6,9 @@ import groovy.json.JsonBuilder
 import groovy.json.JsonOutput
 import jenkins.plugins.http_request.*
 import java.net.URL
-import com.compuware.devops.*
+import com.compuware.devops.config.*
+import com.compuware.devops.jclskeleton.*
+import com.compuware.devops.util.*
 
 /**
 Call method to execute the pipeline from a shared library
