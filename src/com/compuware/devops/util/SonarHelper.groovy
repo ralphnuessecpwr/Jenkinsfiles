200e86088d237e22a270796eb2f287748c995dea
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2019-09-05 18:52:35 +0200

    define

--- src/com/compuware/devops/util/SonarHelper.groovy
+++ src/com/compuware/devops/util/SonarHelper.groovy
@@ -66,6 +66,7 @@ class SonarHelper implements Serializable {
     {
         def scanType            = ''
         def scanProjectName     = ''
+        def scanProjectName     = ''
         def scanTestPath        = ''
         def scanResultPath      = ''
         def scanCoveragePath    = ''
