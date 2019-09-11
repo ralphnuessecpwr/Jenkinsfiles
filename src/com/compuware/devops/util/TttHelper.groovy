ee607342495f3308e3ee75e5bec882ea27e56039
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2019-09-11 20:00:35 +0200

    It's getting late

--- src/com/compuware/devops/util/TttHelper.groovy
+++ src/com/compuware/devops/util/TttHelper.groovy
@@ -95,9 +95,9 @@ class TttHelper implements Serializable {
             def listOfScenarioFullPaths = steps.findFiles(glob: '**/'+ it + '*.xactx')
 
             steps.echo "List:" + listOfScenarioFullPaths.toString()
-            steps.echo listOfScenarioFullPaths.size.toString()
+            steps.echo listOfScenarioFullPaths.size().toString()
 
-            if(listOfScenarioFullPaths.size > 0)
+            if(listOfScenarioFullPaths.size() > 0)
             {
                 steps.totaltest credentialsId:          "${pConfig.hciTokenId}", 
                     environmentId:                      "${pConfig.xaTesterEnvId}", 
