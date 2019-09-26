048852b740b6cc45d9e730910c2d1ac93295f3de
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2019-09-26 08:36:46 +0200

    echo build result

--- vars/ispwGeneratePipeline.groovy
+++ vars/ispwGeneratePipeline.groovy
@@ -233,6 +233,9 @@ def call(Map pipelineParams)
          
             tttHelper.passResultsToJunit()
 
+            echo "After JUnit current Result: " + currentBuild.currentResult
+            echo "After JUnit current Result: " + currentBuild.result
+
             /* push results back to GitHub */
             gitHelper.pushResults(pConfig.gitProject, pConfig.gitTttUtRepo, pConfig.tttFolder, pConfig.gitBranch, BUILD_NUMBER)
         }
