f18e7b658dde655389b87ace26f45e7064f38018
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2020-07-09 17:02:21 +0200

    Release Pipeline

---
+++ src/com/compuware/devops/util/IspwReleaseConfigurator.groovy
@@ -0,0 +1,198 @@
+package com.compuware.devops.util
+
+import groovy.json.JsonSlurper
+import jenkins.plugins.http_request.*
+
+/* Wrapper class to simplify use of ISPW functions */
+class IspwReleaseConfigurator implements Serializable{
+
+    def steps
+    def pConfig
+    def assignmentList
+
+    IspwReleaseConfigurator(steps, pConfig, assignmentListString){
+
+        this.steps              = steps
+        this.pConfig            = pConfig
+        this.assignmentList     = assignmentListString.split(',').collect{it.trim() as String}
+
+    }
+
+    private createRelease()
+    {
+        def mailMessagePart
+
+        try
+        {
+            ispwOperation(
+                connectionId:           pConfig.hci.connectionId, 
+                consoleLogResponseBody: true, 
+                credentialsId:          pConfig.ces.jenkinsToken, 
+                ispwAction:             'CreateRelease', 
+                ispwRequestBody: """
+                    stream=${pConfig.ispw.stream}
+                    application=${pConfig.ispw.application}
+                    releaseId=${pConfig.ispw.release}
+                    description=Default Description
+                    """
+            )
+            
+            "Created release " + pConfig.ispw.release + ".\n"
+        }
+        catch (IllegalStateException e)
+        {
+            "Release " + pConfig.ispw.release + " already existed. Assignments were added.\n"
+        }
+
+        return mailMessagePart
+    }    
+
+    private addAssignments()
+    {
+        assignmentList.each
+        {
+            def currentAssignment   = it
+
+            def response            = httpRequest(
+                url:                        "${pConfig.ispw.url}/ispw/${pConfig.ispw.runtime}/assignments/${it}/tasks",
+                consoleLogResponseBody:     true, 
+                customHeaders:              [[
+                                            maskValue:  true, 
+                                            name:       'authorization', 
+                                            value:      "${cesToken}"
+                                            ]]
+                
+                )
+
+            def taskList            = new JsonSlurper().parseText(response.getContent()).tasks
+
+            def componentList       = []
+
+            taskList.each
+            {
+                componentList.add(it.moduleName)        
+            }
+
+            taskList = null
+
+            componentList.each
+            {
+                echo "Task " + it
+                
+                httpRequest(
+                    httpMode:                   'POST',
+                    url:                        "${pConfig.ispw.url}/ispw/${pConfig.ispw.runtime}/assignments/${currentAssignment}/tasks/transfer?mname=${it}",
+                    consoleLogResponseBody:     true, 
+                    contentType:                'APPLICATION_JSON', 
+                    requestBody:                '''{
+                                                    "runtimeConfiguration": "''' + pConfig.ispw.runtime + '''",
+                                                    "containerId": "''' + pConfig.ispw.release + '''",
+                                                    "containerType": "R"
+                                                }''',
+                    customHeaders:              [[
+                                                maskValue:  true, 
+                                                name:       'Authorization', 
+                                                value:      cesToken
+                                                ]]
+                )
+            }
+
+            return "Added all tasks in assignment " + currentAssignment + " to Release " + pConfig.ispw.release + ".\n"
+        }
+    }
+
+    private removeAssignments()
+    {
+        assignmentList.each
+        {
+            def currentAssignment   = it
+
+            def response            = httpRequest(
+                url:                        "${pConfig.ispw.url}/ispw/${pConfig.ispw.runtime}/assignments/${it}/tasks",
+                consoleLogResponseBody:     true, 
+                customHeaders:              [[
+                                            maskValue:  true, 
+                                            name:       'authorization', 
+                                            value:      "${cesToken}"
+                                            ]]
+                
+                )
+
+            def taskList            = new JsonSlurper().parseText(response.getContent()).tasks
+
+            def componentList       = []
+
+            taskList.each
+            {
+                componentList.add(it.moduleName)        
+            }
+
+            taskList = null
+
+            componentList.each
+            {
+                echo "Task " + it
+                
+                httpRequest(
+                    httpMode:                   'POST',
+                    url:                        "${pConfig.ispw.url}/ispw/${pConfig.ispw.runtime}/releases/${pConfig.ispw.release}/tasks/remove?mname=${it}",
+                    consoleLogResponseBody:     true, 
+                    contentType:                'APPLICATION_JSON', 
+                    requestBody:                '''{
+                                                    "runtimeConfiguration": "''' + pConfig.ispw.runtime + '''"
+                                                }''',
+                    customHeaders:              [[
+                                                maskValue:  true, 
+                                                name:       'Authorization', 
+                                                value:      cesToken
+                                                ]]
+                )
+            }
+
+            return "Removed all tasks in assignment " + currentAssignment + " from Release " + pConfig.ispw.release + ".\n"
+        }
+    }    
+
+    private checkReleaseReady()
+    {
+        def releaseReady        = true
+
+        def failAssignmentList  = []
+
+        def response        = httpRequest(
+            url:                        "${pConfig.ispw.url}/ispw/${pConfig.ispw.runtime}/releases/${pConfig.ispw.release}/tasks",
+            consoleLogResponseBody:     true, 
+            customHeaders:              [[
+                                        maskValue:  true, 
+                                        name:       'authorization', 
+                                        value:      "${cesToken}"
+                                        ]]
+            
+            )
+
+        def taskList        = new JsonSlurper().parseText(response.getContent()).tasks
+
+        taskList.each
+        {
+            if(
+                it.level == 'DEV1' ||
+                it.level == 'DEV2' ||
+                it.level == 'DEV3'
+            )
+            {
+                releaseReady     = false
+
+                if(!failAssignmentList.contains(it.container))
+                {
+                    failAssignmentList.add(it.container)
+                }
+            }
+            else
+            {
+                pConfig.ispw.targetLevel = it.level
+            }
+        }
+
+        return failAssignmentList
+    }
+}
---
+++ vars/ispwComposeReleasePipeline.groovy
@@ -0,0 +1,160 @@
+import groovy.json.JsonSlurper
+import com.compuware.devops.util.*
+
+/**
+ Helper Methods for the Pipeline Script
+*/
+PipelineConfig          pConfig         // Pipeline configuration parameters
+IspwHelper              ispwHelper      // Helper class for interacting with ISPW
+XlrHelper               xlrHelper       // Helper class for interacting with XLRelease
+IspwReleaseConfigurator releaseConfigurator
+
+String                  cesToken                // Clear text token from CES
+String                  mailMessageExtension
+
+private initialize(pipelineParams)
+{
+    // Clean out any previously downloaded source
+    dir(".\\") 
+    {
+        deleteDir()
+    }
+
+    /* Read list of mailaddresses from "private" Config File */
+    /* The configFileProvider creates a temporary file on disk and returns its path as variable */
+    def mailListlines
+
+    configFileProvider(
+        [
+            configFile(
+                fileId: 'MailList', 
+                variable: 'mailListFilePath'
+            )
+        ]
+    ) 
+    {
+        File mailConfigFile = new File(mailListFilePath)
+
+        if(!mailConfigFile.exists())
+        {
+            error "File - ${mailListFilePath} - not found! \n Aborting Pipeline"
+        }
+
+        mailListlines = mailConfigFile.readLines()
+    }
+
+    pipelineParams.ISPW_Assignment  = ''
+    pipelineParams.ISPW_Set_Id      = ''
+    pipelineParams.ISPW_Owner       = pipelineParams.User_Id
+    pipelineParams.ISPW_Src_Level   = 'DEV1'
+
+    // Instantiate and initialize Pipeline Configuration settings
+    pConfig = new PipelineConfig(
+        steps, 
+        workspace,
+        pipelineParams,
+        mailListlines
+    )
+
+    pConfig.initialize()                                            
+
+    // Use Jenkins Credentials Provider plugin to retrieve CES token in clear text from the Jenkins token for the CES token
+    // The clear text token is needed for native http REST requests against the ISPW API
+    withCredentials(
+        [string(credentialsId: "${pConfig.ces.jenkinsToken}", variable: 'cesTokenTemp')]
+    ) 
+    {
+        cesToken = cesTokenTemp
+    }
+
+    // Instanatiate and initialize the ISPW Helper
+    ispwHelper  = new IspwHelper(
+        steps, 
+        pConfig
+    )
+
+    releaseConfigurator = new IspwReleaseConfigurator(
+        steps,
+        pConfig,
+        pipelineParams.ISPW_Assignment_List
+    )    
+
+    mailMessageExtension = ''
+}
+
+
+/**
+Call method to execute the pipeline from a shared library
+@param pipelineParams - Map of paramter/value pairs
+*/
+def call(Map pipelineParams)
+{
+    node
+    {
+        stage("Initialization")
+        {
+            initialize(pipelineParams) 
+            echo "Determined"
+            echo "Application   :" + pConfig.ispw.application
+            echo "Release       :" + pConfig.ispw.release
+        }
+                
+        /* Download all sources that are part of the container  */
+        stage("Perform Action")
+        {
+            switch(pipelineParams.Release_Action) 
+            {
+                case "create Release":
+                    mailMessageExtension = mailMessageExtension + releaseConfigurator.createRelease()
+                    mailMessageExtension = mailMessageExtension + releaseConfigurator.addAssignments()
+                break
+
+                case "add Assignments":
+                    mailMessageExtension = mailMessageExtension + releaseConfigurator.addAssignments()
+                break
+
+                case "remove Assignments":
+                    mailMessageExtension = mailMessageExtension + releaseConfigurator.removeAssignments()
+                break
+
+                case "trigger Release":
+
+                    def failAssignmentList = releaseConfigurator.checkReleaseReady()
+
+                    if(failAssignmentList.size() == 0)
+                    {
+                        // Instantiate and initialize the XLR Helper
+                        xlrHelper   = new XlrHelper(steps, pConfig)
+
+                        xlrHelper.triggerRelease()
+
+                        mailMessageExtension = mailMessageExtension + "Triggered XL Release for " + pConfig.ispw.release + ".\n"
+                    }
+                    else
+                    {
+                        mailMessageExtension = mailMessageExtension + "Some assignments for Release " + pConfig.ispw.release + "still contain tasks in development.\n" +
+                            "The release cannot be triggered. Either remove those tasks from the assignments or remove the assignments from the release:\n\n"
+
+                        failAssignmentList.each
+                        {
+                            mailMessageExtension = mailMessageExtension + it + "\n"
+                        }
+                    }
+
+                break
+
+                default:
+                    echo "Wrong Action Code"
+                break
+            }
+        }
+
+        stage("Send Notifications")
+        {
+            emailext subject:       'Performed Action ' + pipelineParams.Release_Action + ' on Release ' + pConfig.ispw.release,
+            body:       mailMessageExtension,
+            replyTo:    '$DEFAULT_REPLYTO',
+            to:         "${pConfig.mail.recipient}"
+        }
+    }
+}
