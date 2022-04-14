f6955fe93ecc13cf50561d4fa6bd3d80f3bdb64f
Ralph Nuesse <ralph_nuesse@bmc.com>
2022-04-14 15:22:39 +0200

    Run Compare

--- vars/Mddl_Generate_Pipeline.groovy
+++ vars/Mddl_Generate_Pipeline.groovy
@@ -37,15 +37,14 @@ def call(Map execParms)
         stage("Process MDDL members") {
 
             mddlTaskContentList = getMddlTaskContentList()
-            println mddlTaskContentList.toString()
-            println mddlTaskContentList[0].toString()
 
         }
 
         stage('Compare Developer Schema definition against Production') {
 
             def mddlTaskContent = mddlTaskContentList[0]
-            println mddlTaskContent.toString()
+            def workIdOwner     = mddlTaskContent.userId
+            def workIdName      = mddlTaskContent.moduleName
 
             withCredentials(
                 [   
@@ -68,6 +67,135 @@ def call(Map execParms)
                     symdir:     pipelineConfig.amiDevOps.symDir
                 )
             }
+
+            bmcAmiDb2SchemaChangeMigration(
+                acceptableRC: '0000', 
+                jobWaitTime: 2, 
+                moduletype: 'compare3', 
+                nocdl: false, 
+                objtyp: 'TS', 
+                location2: mddlTaskContent.DB2PSSID,
+                ssid: mddlTaskContent.DB2SSID,
+                objPart1C1: mddlTaskContent.DB2DB, 
+                objPart1C2: mddlTaskContent.DB2PDB, 
+                objPart2C1: mddlTaskContent.DB2TS, 
+                objPart2C2: mddlTaskContent.DB2PTS, 
+                objPart3C1: '', 
+                objPart3C2: '', 
+                postbaseexec: false, 
+                postbasename: '', 
+                postbaseprof: '', 
+                preBaseType: 'none', 
+                prebasename: '', 
+                prebaseprof: '', 
+                useCrule: false, 
+                useCruleAfter: false, 
+                useCruleBefore: false, 
+                wkidname: workIdName, 
+                wkidowner: workIdOwner, 
+                wlistpds: 'HDDRXM0.DEMO.JCL(AMIWL)'
+                cdlRollCheck: false, 
+                cdlRollPds: '', 
+                cdlpds: 'HDDRXM0.DEMO.JCL(AMICDL)', 
+                cmpbl1: '', 
+                cmpbl2: '', 
+                cmpbp1: '', 
+                cmpbp2: '', 
+                cmpddl1: '', 
+                cmpddl2: '', 
+                crule: '', 
+                crule1: '', 
+                crule2: '', 
+                cruleAfter: '', 
+                cruleBefore: '', 
+                debug: false, 
+                disablebuildstep: false, 
+                execjclpds: 'HDDRXM0.DEMO.JCL(AMIEXEC)', 
+                genjcl: false, 
+                imprptpds: '',                 
+                analysisin: '''//*Analysis Input
+//ANALYSIS.ALURPT DD DISP=SHR,
+// DSN=&IMPRPT
+//ANALYSIS.ALUIN DD *
+  SSID  ${SSID}
+  WORKID  ${Work ID Owner}.${Work ID Name}
+  INCLUDE (DATA AMS SQL )''', 
+                compin: '''//*Compare Input
+//COMPARE1 EXEC AMAPROCC,
+// COND=(1,LT)
+//COMPARE.CDL001 DD DISP=SHR,
+// DSN=&CDLFILE
+//COMPARE.ALUIN DD *
+     SSID  ${SSID}
+     CMPTYPE1 LOCAL
+     CMPTYPE2 LOCAL
+     INCLUDE (ALTER CREATE DROP )
+     REPORT (DETAIL )
+     SCOPETYPE RULE
+     SCOPERULE1 (* ${Object Type (applies to Compare1 and Compare2)} ${Object Name Part 1 for Compare1}.${Object Name Part 2 for Compare1})
+     SCOPERULE2 (* ${Object Type (applies to Compare1 and Compare2)} ${Object Name Part 1 for Compare2}.${Object Name Part 2 for Compare2})
+     NOSYNONYM
+     NOALIAS
+     NODEFINE''', 
+                impin: '''//*Import Input
+//IMPORT.ALUIN DD *
+  SSID ${SSID}
+  REPLACEWORKID  ${Work ID Owner}.${Work ID Name}
+  SOURCETYPE CDL''', 
+                jclgenin: '''//*JCLGEN Input
+//STEP1.AJXIN DD *
+  PRODUCT  ACM
+  WORKID   ${Work ID Owner}.${Work ID Name}
+  SSID     ${SSID}''', 
+                jcomp: '''${Job Card}
+//*
+//        JCLLIB ORDER=(#proclib#)
+//*
+//JOBLIB  DD  DSN=#joblib1#,DISP=SHR
+//        DD  DSN=#joblib2#,DISP=SHR
+//        DD  DSN=#dsnexit#,DISP=SHR
+//        DD  DSN=#dsnload#,DISP=SHR
+//***************************************************
+// SET ACMDOPT=#dopts#
+// SET RTEHLQ=#rtehlq#
+// SET USRHLQ=#usrhlq#
+//***************************************************
+// SET DDLFILE=${Compare2 DDL PDS and Member Name}
+// SET CDLFILE=${CDL PDS and Member Name}
+// SET WORKLIST=${Worklist PDS and Member Name}
+// SET EXECJCL=${Execution JCL PDS and Member Name}
+// SET IMPRPT=${Impact Report PDS and Member Name}
+//*
+// SET POFFILE=#pofdsn#
+//*----------------------------------------------------
+//* BMC CHANGE MANAGER COMPARE
+//*----------------------------------------------------
+${Compare Input Stream}
+//*----------------------------------------------------
+//* BMC CHANGE MANAGER IMPORT
+//*----------------------------------------------------
+//IMPORT   EXEC AMAPROCI,
+//    COND=${BMC_COMPARE_COND_CODE}
+//IMPORT.IMPORTIN DD DISP=SHR,
+//  DSN=&CDLFILE
+${Import Input Stream}
+//*----------------------------------------------------
+//* BMC CHANGE MANAGER ANALYSIS
+//*----------------------------------------------------
+//ANALYSIS EXEC AMAPROCA,
+//    COND=${BMC_COMPARE_COND_CODE}
+${Analysis Input Stream}
+//*----------------------------------------------------
+//* BMC JCL GENERATION
+//*----------------------------------------------------
+//JCLGEN   EXEC AMAPROCJ, 
+//    COND=${BMC_COMPARE_COND_CODE}
+${JCL Generation Input Stream}''', 
+                jobCardIn: '''//HDDRXM0J JOB (EUDD),\'COMPARE-${Job_ID}\',
+//  CLASS=A,MSGLEVEL=(1,1)                
+//*                                       
+//*                                       
+//*'''
         }
     }
 }
@@ -181,6 +309,7 @@ def getMddlTaskContentList() {
 
         mddlTaskContent['taskId']       = it.taskId
         mddlTaskContent['moduleName']   = it.moduleName
+        mddlTaskContent['userId']       = it.userId
         
         mddlTaskContentList.add(mddlTaskContent)
     
