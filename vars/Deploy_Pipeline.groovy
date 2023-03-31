a3d887de956f9713d64334043b9e84773304b2d9
Ralph <ralph_nuesse@bmc.com>
2023-03-31 11:21:41 +0200

    Deploy

---
+++ vars/Deploy_Pipeline.groovy
@@ -0,0 +1,105 @@
+import groovy.json.JsonSlurper
+
+String configFile
+String ispwSourceLevel
+String ispwTargetLevel
+String cesUrl
+def pipelineConfig
+def symdir
+def mddlTaskList
+
+def call(Map execParms)
+{
+    configFile  = 'mddlPipeline.yml'
+
+    node {
+
+        stage("Initialize"){
+
+            initialize(execParms)
+
+        }
+
+        echo "Calling Mddl_Promote_Pipeline with parameters\n\n" +
+            "execParms: " + execParms.toString() + "\n\n" +
+            "pipelineConfig: " + pipelineConfig.toString() + "\n\n" +
+            "mddlTaskList: " + mddlTaskList.toString() + "\n\n" +
+            "ispwCurrentLevel: "  + ispwCurrentLevel + "\n\n" + 
+            "db2SourceLevel: "  + db2SourceLevel + "\n\n" + 
+            "db2TargetLevel: " + db2TargetLevel + "\n\n" + 
+            "cesUrl: " + cesUrl
+
+        Mddl_Deploy_Pipeline(execParms, pipelineConfig, mddlTaskList, ispwCurrentLevel, db2SourceLevel, db2TargetLevel, cesUrl)
+    }
+}
+
+def initialize(execParms) {
+
+    cleanWs()
+
+    pipelineConfig      = readYaml(text: libraryResource(configFile))
+
+    ispwStream          = execParms.ispwStream
+    ispwApplication     = execParms.ispwApplication
+    ispwAssignmentId    = execParms.ispwAssignmentId
+    ispwReleaseId       = execParms.ispwReleaseId
+    ispwOwner           = execParms.ispwOwner
+    ispwLevel           = execParms.ispwLevel
+    cesUrl              = pipelineConfig.ces.hostName + ':' + pipelineConfig.ces.port
+
+    def taskList        = getTaskList(ispwReleaseId)
+    cobTaskList         = getTaskListsByType(taskList)[0]
+    mddlTaskList        = getTaskListsByType(taskList)[1]
+
+    if(mddlTaskList.size() > 1) {
+
+        error "At the current stage we do not support processing of more than one MDDL member per build."
+
+    }
+    else if(mddlTaskList.size() == 0) {
+        error "No MDDL Task was found."
+    }
+
+    ispwCurrentLevel    = ispwLevel
+    db2SourceLevel      = pipelineConfig.ispw.lifeCycle[ispwLevel].db2SourceLevel
+    db2TargetLevel      = pipelineConfig.ispw.lifeCycle[ispwLevel].db2TargetLevel    
+}
+
+def getTaskList(releaseId) {
+
+    def response    = ispwOperation(
+                            connectionId:           pipelineConfig.host.connectionId, 
+                            credentialsId:          pipelineConfig.ces.credentialsId,
+                            serverConfig:           pipelineConfig.ispw.runtimeConfig, 
+                            consoleLogResponseBody: true, 
+                            ispwAction:             'GetReleaseTaskList', 
+                            ispwRequestBody:        'releaseId=' + releaseId
+                        )
+
+    def taskList        = readJSON(text: response.content).tasks
+
+    return taskList
+}
+
+def getTaskListsByType(taskList) {
+    
+    def taskListsByType = []
+    def mddlTaskList    = []
+    def cobTaskList     = []
+
+    taskList.each() {
+
+        if(it.moduleType == pipelineConfig.ispw.mddlType){
+            mddlTaskList.add(it)
+        }
+        else if(it.moduleType == pipelineConfig.ispw.cobType){
+            cobTaskList.add(it)
+        }
+
+    }
+
+    taskListsByType.add(cobTaskList)
+    taskListsByType.add(mddlTaskList)
+
+    return taskListsByType
+}
